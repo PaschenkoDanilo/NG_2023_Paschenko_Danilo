39a81216af945022774d7615937c2aed6147a35b
Пользователь <pachenkomih@gmail.com>
2022-10-18 15:13:27 +0300

    Simplifield code in task 2

--- Python/Lesson_1/task2.py
+++ Python/Lesson_1/task2.py
@@ -1,31 +1,27 @@
 from math import sqrt
 
-
 print ("Hello! It is calculator")
 print ("Math operators available: +, -, *, /, ^, square root(input: sqrt)")
 mathOperator = input("Input math operator which you want: ")
-
-if mathOperator == "sqrt":
+if mathOperator == "^":
+    numberForExponentiation = int(input("Input number: "))
+    powerOfNumber = int(input("Input power of number: "))
+    print ("Result: " + str(numberForExponentiation** powerOfNumber))
+elif mathOperator == "sqrt":
     numberForSqrt = int(input("Input number: "))
-    print ("Result:" , end = " ")
-    print (sqrt(numberForSqrt))
+    print ("Result:" + str(sqrt(numberForSqrt)))
 else:
-    if mathOperator == "^":
-        numberForExponentiation = int(input("Input number: "))
-        powerOfNumber = int(input("Input power of number: "))
-        print ("Result:" , end = " ")
-        print (numberForExponentiation ** powerOfNumber)
-    else:
-        numberOne = int(input("Input number one: "))
-        numberTwo = int(input("Input number two: "))
-        print ("Result:" , end = " ")
+    numberOne = int(input("Input number one: "))
+    numberTwo = int(input("Input number two: "))
+    print ("Result:" , end = " ")
+    if mathOperator == "+":
+        print (numberOne + numberTwo)
+    if mathOperator == "-":
+        print (numberOne - numberTwo)
+    if mathOperator == "*":
+        print (numberOne * numberTwo)
+    if mathOperator == "/":
+        print (numberOne / numberTwo)
+    
 
-        if mathOperator == "+":
-            print (numberOne + numberTwo)
-        if mathOperator == "-":
-            print (numberOne - numberTwo)
-        if mathOperator == "*":
-            print (numberOne * numberTwo)
-        if mathOperator == "/":
-            print (numberOne / numberTwo)
     

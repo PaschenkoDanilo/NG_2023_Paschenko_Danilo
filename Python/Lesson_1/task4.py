85b564a753d22b9df97ff10b412eafda8054c399
Пользователь <pachenkomih@gmail.com>
2022-10-21 17:37:27 +0300

    Added task 4 (Python)

---
+++ Python/Lesson_1/task4.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+print ("Input coefficients of quadratic equation.")
+quadraticCoefficient = int(input("Input quadratic coefficient: "))
+linearCoefficient = int(input("Input linear coefficient: "))
+constantTerm = int(input("Input constant term: "))
+
+discriminant = linearCoefficient**2 - 4*quadraticCoefficient*constantTerm
+
+if discriminant < 0:
+    print ("Negative discriminant. Quadratic equation haven't real roots.")
+elif discriminant == 0:
+    root = (-linearCoefficient / 2*quadraticCoefficient)
+    print ("Real root = " + str(root))
+else:
+    firstRoot = (-linearCoefficient + sqrt(discriminant)) / (2*quadraticCoefficient)
+    secondRoot = (-linearCoefficient - sqrt(discriminant)) / (2*quadraticCoefficient)
+    print ("First real root = " + str(firstRoot) + "\n" + "Second real root = " + str(secondRoot))

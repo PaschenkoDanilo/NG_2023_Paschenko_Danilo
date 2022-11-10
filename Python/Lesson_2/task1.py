e7320b27b820c84a76214a27a322a19a553ede3b
Пользователь <pachenkomih@gmail.com>
2022-11-10 23:30:29 +0200

    Modified task1 (Changed the code that counts the number of letters and sorts

--- Python/Lesson_2/task1.py
+++ Python/Lesson_2/task1.py
@@ -1,32 +1,10 @@
 text = input("Input text: ")
 uniqueText = set(text)
-lettersCount = []
+lettersCount = {}
 
-for letters in uniqueText:
-    count = 0
-    for eachLetters in text:
-        if letters == eachLetters:
-            count+=1
-    lettersCount.extend([letters + " - " + str(count)])
+for unique in uniqueText:
+    lettersCount[unique] = text.count(unique)
+print ("Count of letters: " + str(lettersCount) + "\n")
 
-
-
-
-lettersCountString = ", ".join(lettersCount)
-print("Count of letters in text: " + lettersCountString + "\n")
-
-lettersCount.sort()
-sortedByLetters = ", ".join(lettersCount)
-print ("Sorted text by letters: " + sortedByLetters + "\n")
-
-lettersCountSortedByAmount = []
-for letters in uniqueText:
-    count = 0
-    for eachLetters in text:
-        if letters == eachLetters:
-            count+=1
-    lettersCountSortedByAmount.extend([str(count) + " - " + letters])
-
-lettersCountSortedByAmount.sort(reverse = True)
-sortedByAmount = ", ".join(lettersCountSortedByAmount)
-print ("Sorted text by amount of letters: " + sortedByAmount)
+print ("Sorted by letters: " + str(sorted(lettersCount.items())))
+print ("Sorted by amount: " + str(sorted(lettersCount.items(), key = lambda x: x[1], reverse = True)))

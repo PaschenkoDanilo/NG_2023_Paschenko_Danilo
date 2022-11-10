b19c1427f5632a988b362037de0a47302afbd12a
Пользователь <pachenkomih@gmail.com>
2022-11-10 23:53:21 +0200

    Modified task1 (Deleted indentations between function blocks and replaced the if/else function with match/case

--- Python/Lesson_3/task1.py
+++ Python/Lesson_3/task1.py
@@ -4,35 +4,32 @@ def main ():
     print ("Hello! It is calculator.")
     print ("Math operators available: +, -, *, /, ^, square root(input: sqrt).")
     mathOperator = input("Input math operator which you want: ")
-    if mathOperator == "^":
-        exponentation ()
-    elif mathOperator == "sqrt":
-        quadraticRoot ()
-    else: defaultMathOperation (mathOperator)
-
-
+    match mathOperator:
+        case "^":
+            exponentation ()
+        case "sqrt":
+            quadraticRoot ()
+        case _: 
+            defaultMathOperation (mathOperator)
 def exponentation ():
     numberForExponentiation = int(input("Input number: "))
     powerOfNumber = int(input("Input power of number: "))
     print ("Result: " + str(numberForExponentiation ** powerOfNumber))
-
 def quadraticRoot ():
     numberForSqrt = int(input("Input number: "))
     print ("Result:" + str(sqrt(numberForSqrt)))
-
 def defaultMathOperation (mathOperator):
     numberOne = int(input("Input number one: "))
     numberTwo = int(input("Input number two: "))
     print ("Result:" , end = " ")
-    if mathOperator == "+":
-        print (numberOne + numberTwo)
-    elif mathOperator == "-":
-        print (numberOne - numberTwo)
-    elif mathOperator == "*":
-        print (numberOne * numberTwo)
-    else:
-        print (numberOne / numberTwo)
-
-
+    match mathOperator:
+        case "+":
+            print (numberOne + numberTwo)
+        case "-":
+            print (numberOne - numberTwo)
+        case "*":
+            print (numberOne * numberTwo)
+        case _:
+            print (numberOne / numberTwo)
 
 main ()

e8ec2afaefc8b6933af6ee1e0f5a3dec3c6db94a
Пользователь <pachenkomih@gmail.com>
2023-01-27 02:57:37 +0200

    Lesson 3 Task 3. Count number of letters in string without cycles. Completed the task using only one function.

--- Python/Lesson_3/task3.py
+++ Python/Lesson_3/task3.py
@@ -1,18 +1,12 @@
 text = input("Entire your string: ").lower()
 lettersDict = {}
 
-def equatingToOne(position):
-    if len(text) > position:
-        lettersDict[text[position]] = 0
-        return equatingToOne(position + 1)
-    else:
-        return lettersDict
-
 def lettersCount(position, lettersDict):
-    if position == 0:
-        equatingToOne(0)
     if len(text) > position:
-        lettersDict[text[position]] += 1
+        try:
+            lettersDict[text[position]] += 1
+        except:
+            lettersDict[text[position]] = 1
         return lettersCount(position + 1, lettersDict)
     else:
         print(lettersDict)

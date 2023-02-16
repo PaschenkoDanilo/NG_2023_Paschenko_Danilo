d08a8f9ec8a012c02b0f35672de2bbdb13efb1d4
Пользователь <pachenkomih@gmail.com>
2023-02-16 20:00:13 +0200

    Reworked the function of encrypting text into a picture (for PNG, BMP and JPG/JPEG, ver: 2.0

--- Python_Project/lsbEncode.py
+++ Python_Project/lsbEncode.py
@@ -1,62 +1,45 @@
 from PIL import Image, ImageDraw
 from random import randint
 
-img = Image.open("start.bmp")
-draw = ImageDraw.Draw(img)
-pix = img.load()
+with Image.open("start.bmp") as img:
+    draw = ImageDraw.Draw(img)
+    pix = img.load()
+    width = img.size[0]
+    height = img.size[1]
+    keys = open("keys.txt", "w+")
 
-width = img.size[0]
-heigth = img.size[1]
-
-keys = open("keys.txt", "w+")
+text = "rol kek LOL kekich rofls KEKWait"
 
 
-text = "rol kek LOL kekich rofls KEKWait, LMAOfdjfgjlgfojdfhjdfhjhhjhdfgjldlgjkdfghldkfjglsfghfhfghdhdfufgufuhguhfguhfuhfghfuhghfghfghkdf;strhhrt74545477shugrhb "
+
 imgMask = 0b11111100
 textMask = 0b11000000
-
+indexForRGB = [0, 2, 0, 2]
 
 def encoder():
-    for symbol in text:
-        letter = ord(symbol)
-        encodeFirstHalf(letter)
-        encodeSecondHalf(letter)
+    index = 0
+    while index <= len(text) - 1:
+        letter = ord(text[index])
+        key = randint(0, width - 10), randint(0, height - 10)
+        for i in range(0, 4):
+            colorCode = pix[key][indexForRGB[i]] & imgMask
+            letterMask = letter & textMask
+            letterMask >>= 6
+            colorCode |= letterMask
+            encodeInImage(colorCode, key, i)
+            letter <<= 2
+            if i == 1:
+                key = randint(0, width - 10), randint(0, height - 10)
+        index += 1
     img.save("newstart.bmp", "BMP")
     keys.close()
 
-def encodeFirstHalf(letter):
-    key = randint(0, width - 10),randint(0, heigth - 10)
-    red = pix[key][0] & imgMask
-    letterMask = letter & textMask
-    letterMask >>= 6
-    red |= letterMask
-    letter <<= 2
+def encodeInImage(colorCode, key, index):
+    if index == 0 or index == 2:
+        draw.point(key, (colorCode, pix[key][1], pix[key][2]))
+    elif index == 1 or index == 3:
+        draw.point(key, (pix[key][0], pix[key][1], colorCode))
+    if index == 1 or index == 3:
+        keys.write(str(key) + "\n")
 
-    blue = pix[key][2] & imgMask
-    letterMask = letter & textMask
-    letterMask >>= 6
-    blue |= letterMask
-    encodeInFile(key, red, blue)
-
-def encodeSecondHalf(letter):
-    key = randint(0, width - 10),randint(0, heigth - 10)
-    red = pix[key][0] & imgMask
-    letter <<= 4
-    letterMask = letter & textMask
-    letterMask >>= 6
-    red |= letterMask
-    letter <<= 2
-
-    blue = pix[key][2] & imgMask
-    letterMask = letter & textMask
-    letterMask >>= 6
-    blue |= letterMask
-    encodeInFile(key, red, blue)
-
-def encodeInFile(key, red, blue):
-    green = pix[key][1]
-    draw.point(key, (red, green, blue))
-    keys.write(str(key) + "\n")
-
-    
 encoder()
